293675acae64c97b9530abb2fabe74feb079cccc
Floris Schippers <FlorisSchippers@gmail.com>
2019-02-19 11:15:49 +0100

    Initial commit

---
+++ webroot/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" href="favicon.png">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+	<link rel="stylesheet" type="text/css" href="fullpage.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<title>Test</title>
+</head>
+<body>
+	<div id="fullpage">
+		<div class="section active" id="section0"><h1>fullPage.js</h1></div>
+		<div class="section" id="section1">
+			<div class="slide "><h1>Simple Demo</h1></div>
+			<div class="slide active"><h1>Only text</h1></div>
+			<div class="slide"><h1>And text</h1></div>
+			<div class="slide"><h1>And more text</h1></div>
+		</div>
+		<div class="section" id="section2"><h1>No wraps, no extra markup</h1></div>
+		<div class="section" id="section3"><h1>Just the simplest demo ever</h1></div>
+	</div>
+	<script type="text/javascript" src="fullpage.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
